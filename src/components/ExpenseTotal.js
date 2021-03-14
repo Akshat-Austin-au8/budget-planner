8286f97e4960d8a8b17a09628f72f8b14791efce
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-03-14 23:55:03 +0530

    budget app started

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseTotal = () => {
+  return (
+    <div className='alert alert-primary'>
+      <span>Spand so far: ₹2000</span>
+    </div>
+  )
+}
+
+export default ExpenseTotal
